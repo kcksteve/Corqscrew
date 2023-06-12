1464b0c5596d4d7a5cd5c69a4171dd14f56f29ad
steve-krawczyk <129321117+steve-krawczyk@users.noreply.github.com>
2023-06-12 10:47:14 -0400

    Added logging for Grid Caches

--- background.js
+++ background.js
@@ -1,39 +1,4 @@
-const badgeClicked = (loggingStyle) => {
-    let pageData;
-
-    const logPageData = () => {
-        let date = new Date(Date.now()).toLocaleTimeString();
-
-        if (loggingStyle === 'prettier') {
-            let groupLabel = '%cPage Data (' + date + "):"
-            let logStyling = 'color: cyan; font-weight: bold;';
-            console.group(groupLabel, logStyling);
-            console.log(pageData);
-
-            if (pageData.validationErrors.length > 0) {
-                console.warn('Validation errors were found.');
-            }
-
-            if (Object.keys(pageData.integratorErrors).length > 0) {
-                console.warn('Integration errors were found.');
-            }
-
-            console.groupEnd(groupLabel);
-        }
-        else {
-            console.log(pageData);
-        }
-    }
-
-    if (typeof angular != 'undefined' && typeof angular.element('.unqorkio-form').scope().submission != 'undefined') {
-        pageData = angular.element('.unqorkio-form').scope().submission;
-        logPageData();
-    }
-    else {
-        console.log('No unqork page data found.');
-    }
-}
-
+//Get options when run
 let loggingStyle;
 
 chrome.storage.local.get(
@@ -43,12 +8,69 @@ chrome.storage.local.get(
     }
 );
 
+//Watch for option changes
 chrome.storage.onChanged.addListener((changes, area) => {
     if (area === 'local' && changes) {
         loggingStyle = changes.loggingStyle.newValue;
     }
 });
 
+//Log after button is clicked
+const badgeClicked = (loggingStyle) => {
+    let pageData = null;
+    let cacheData = null;
+
+    const logPageData = () => {
+        let date = new Date(Date.now()).toLocaleTimeString();
+
+        if (loggingStyle === 'prettier') {
+            let mainGroupLabel = '%cUnqork Data Found (' + date + '):'
+            let subGroupLabel = '%cSubmission Data'
+            let cacheGroupLabel = '%cCache Data'
+            let logStyling = 'color: cyan; font-weight: bold;';
+            console.group(mainGroupLabel, logStyling);
+            console.group(subGroupLabel, logStyling);
+            console.log(pageData);
+            console.groupEnd(subGroupLabel);
+
+            if (cacheData != null && Object.keys(cacheData).length > 0) {
+                console.group(cacheGroupLabel, logStyling);
+                console.log(cacheData);
+                console.groupEnd(cacheGroupLabel);
+            }
+
+            if (pageData.validationErrors.length > 0) {
+                console.warn('Validation errors were found.');
+            }
+
+            if (Object.keys(pageData.integratorErrors).length > 0) {
+                console.warn('Integration errors were found.');
+            }
+
+            console.groupEnd(mainGroupLabel);
+        }
+        else {
+            console.log(pageData);
+        }
+    }
+
+    //Verify what is available to log
+    if (typeof angular != 'undefined') {
+        if (typeof angular.element(document.body).injector().get("CacheService").cache != 'undefined') {
+            cacheData = angular.element(document.body).injector().get("CacheService").cache
+        }
+
+        if (typeof angular.element('.unqorkio-form').scope().submission != 'undefined') {
+            pageData = angular.element('.unqorkio-form').scope().submission;
+            logPageData();
+        }
+    }
+    else {
+        console.log('No unqork page data found.');
+    }
+}
+
+//Listen for badge click
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target : {tabId : tab.id},
@@ -57,4 +79,3 @@ chrome.action.onClicked.addListener((tab) => {
         world : 'MAIN'
     })
 });
-
